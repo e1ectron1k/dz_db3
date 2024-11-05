86617b2fcc4247cfcd148c920d2d182eaad87480
e1ectron1k <tron1k@yandex.ru>
2024-11-05 19:34:10 +0300

    Update db_3_3.sql
    fix 7,9 and 10 requests

--- db_3_3.sql
+++ db_3_3.sql
@@ -5,12 +5,9 @@ GROUP BY genre;
 
 -- 2 Количество треков, вошедших в альбомы 2019–2020 годов.
 SELECT COUNT(*) AS track_count
-FROM Tracks
-WHERE album_id IN (
-    SELECT id
-    FROM Albums
-    WHERE release_year BETWEEN 2019 AND 2020
-);
+FROM Tracks t
+JOIN Albums a ON t.album_id = a.id
+WHERE a.release_year BETWEEN 2019 AND 2020;
 
 -- 3 Средняя продолжительность треков по каждому альбому.
 SELECT album_id, AVG(duration) AS average_duration
@@ -18,14 +15,20 @@ FROM Tracks
 GROUP BY album_id;
 
 -- 4 Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT DISTINCT a.name
-FROM Artists a
-LEFT JOIN Albums al ON a.id = al.artist_id AND al.release_year = 2020
-WHERE al.id IS NULL;
+SELECT DISTINCT ArtistID, ArtistName
+FROM Artists
+WHERE ArtistID NOT IN (
+    SELECT DISTINCT ArtistID
+    FROM Albums
+    WHERE ReleaseYear = 2020
+   );
+
 
 -- 5 Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
-SELECT c.title
+SELECT DISTINCT c.title
 FROM Compilations c
-JOIN Compilation_Artists ca ON c.id = ca.compilation_id
-JOIN Artists a ON ca.artist_id = a.id
-WHERE a.name = 'Artist A';
+JOIN Compilation_Track ct ON c.id = ct.compilation_id
+JOIN Tracks t ON ct.track_id = t.id
+JOIN Artist_Album aa ON t.album_id = aa.album_id
+JOIN Artists a ON aa.artist_id = a.id
+WHERE a.name = 'Some_Artist_Name'; 
